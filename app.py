885cc2979b569edd38daca9a8eb35114c9333fd7
smedinaf <169066790+smedinaf@users.noreply.github.com>
2026-02-26 10:28:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,14 @@ st.image(image, caption = "Conejito feliz")
 
 texto = st.text_input('Escribe el nombre de tu animal favorito', 'Mi animal fav')
 st.write('El animal favorito del usuario es', texto)
+
+st.subheader("Probemos con 2 columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces mejoran la experiencia del usuario")
+  resp = st.checkbox('De acuerdo')
+  if resp:
+    st.write('Correcto!')
