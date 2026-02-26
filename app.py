ff7728427a770b800885df99c7a7a13d37ac04ee
smedinaf <169066790+smedinaf@users.noreply.github.com>
2026-02-26 10:18:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ st.header("En esta página Sofi está cocinando su primera app")
 st.write("La app se está cocinando")
 image = Image.open('conejito.jpg')
 st.image(image, caption = "Conejito feliz")
+
+
+texto = st.text_input('Escribe el nombre de tu animal favorito', 'Mi animal fav')
+st.write('El animal favorito del usuario es', texto)
