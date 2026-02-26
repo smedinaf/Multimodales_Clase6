d14f98452b837127427b4e35a0f1364062824391
smedinaf <169066790+smedinaf@users.noreply.github.com>
2026-02-26 10:06:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from PIL import Image
 st.title("La mini app de Sofi")
 st.header("En esta página Sofi está cocinando su primera app")
 st.write("La app se está cocinando")
-image = Image.open('conejito')
+image = Image.open('conejito.jpg')
 st.image(Image, caption = "Conejito feliz)
